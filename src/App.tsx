8ef3cc5944f48addcbe5d3e59f06fe11ed29a954
zszillat <zachary.szillat@outlook.com>
2024-11-03 17:38:45 -0500

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import SigninForm from './_auth/forms/SigninForm';
-import { Home } from './_root/pages';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
+import './globals.css';
 
 const App = () => {
   return (
-    <main className='flex h-screen'>
-        <Route>
-            {/* public routes */}
-            <Route element={<AuthLayout/>}>
-                <Route path="/sign-in" element={<SigninForm/>}/>
-                <Route path="/sign-up" element={<SignupForm/>}/>
-            </Route>
-
-
-            {/* private routes*/}
-            <Route element={<RootLayout/>}>
-                <Route index element={<Home/>}/>
-            </Route>
-
-
-
-        </Route>
-    </main>
+    <h1 className="text-3xl font-bold underline">Hello Snapgram</h1>
   )
 }
 
--- src/_auth/forms/SignupForm.tsx
+++
@@ -1,8 +0,0 @@
-
-const SignupForm = () => {
-  return (
-    <div>SignupForm</div>
-  )
-}
-
-export default SignupForm
