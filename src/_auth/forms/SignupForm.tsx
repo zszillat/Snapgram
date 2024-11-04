22ba5bb2a744854ab0c95fc813dd0d7e2fb07174
zszillat <zachary.szillat@outlook.com>
2024-11-03 19:39:31 -0500

    Auth Functionality - Appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -7,6 +7,7 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
 
 
@@ -25,8 +26,8 @@ const SignupForm = () => {
  
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    //const newUser = await createUserAccount(values); 
-    console.log(values)
+    const newUser = await createUserAccount(values); 
+    console.log(newUser);
   }
 
   return (
