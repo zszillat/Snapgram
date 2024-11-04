357156274f2ae82b77da026d8e888251ca9ffc31
zszillat <zachary.szillat@outlook.com>
2024-11-03 19:23:18 -0500

    Auth Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
+import './globals.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
