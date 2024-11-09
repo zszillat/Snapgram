c028f0063f64b96cbb3358cc5d2905123e6fe578
zszillat <zachary.szillat@outlook.com>
2024-11-09 11:36:53 -0500

    Home Page: LeftBar Complete [2:44:07]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
 import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
   Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
 } from "@/_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
@@ -22,9 +30,17 @@ const App = () => {
               <Route path="/sign-up" element={<SignupForm />} />
             </Route>
 
-            {/* Private Routes */}
+            {/* private routes */}
             <Route element={<RootLayout />}>
-                <Route index element={<Home />}/>
+              <Route index element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/saved" element={<Saved />} />
+              <Route path="/all-users" element={<AllUsers />} />
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/update-post/:id" element={<EditPost />} />
+              <Route path="/posts/:id" element={<PostDetails />} />
+              <Route path="/profile/:id/*" element={<Profile />} />
+              <Route path="/update-profile/:id" element={<UpdateProfile />} />
             </Route>
             
         </Routes>
