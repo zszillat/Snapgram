e8f01fca5b85f7ae6038f62c71cfd16c1e3c4db9
zszillat <zachary.szillat@outlook.com>
2024-11-09 11:48:50 -0500

    HomePage Complete

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Link, useLocation } from "react-router-dom";
 
-const BottomBar = () => {
+import { bottombarLinks } from "@/constants";
+
+const Bottombar = () => {
+  const { pathname } = useLocation();
+
   return (
-    <div>BottomBar</div>
-  )
-}
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            key={`bottombar-${link.label}`}
+            to={link.route}
+            className={`${
+              isActive && "rounded-[10px] bg-primary-500 "
+            } flex-center flex-col gap-1 p-2 transition`}>
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`${isActive && "invert-white"}`}
+            />
 
-export default BottomBar
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Bottombar;
